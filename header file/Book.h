06b441929e1029308229b85095b3967f568d1e3d
aeternysdex-web <aeternysdex@gmail.com>
2026-09-24 21:30:51 +0300

    Update Book.h

--- header file/Book.h
+++ header file/Book.h
@@ -2,8 +2,8 @@
 
 #include <string>
 
-// Класс Book описывает один экземпляр издания в сети библиотек.
-class Book {
+class Book 
+{
 private:
     int id;
     std::string title;
@@ -12,12 +12,11 @@ private:
 public:
     Book(int id, const std::string& title, const std::string& author);
 
-    // Получение отдельных характеристик
     int getId() const;
     std::string getTitle() const;
     std::string getAuthor() const;
 
-    // Получение и вывод полной информации об объекте
-    void printInfo() const;   // ID, название, автор
-    void printShort() const;  // короткая строка для списков
+ 
+    void printInfo() const;   
+    void printShort() const;  
 };
